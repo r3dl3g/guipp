347f6d96da571a17f216cb83b7bd0d60f9438392
Armin Rothfuss <armin@rothfuss@-web.de>
2016-06-23 16:24:12 +0200

    Work in progress on X11.

---
+++ gui/draw/sources/brush.cpp
@@ -0,0 +1,133 @@
+/**
+* @copyright (c) 2015-2016 Ing. Buero Rothfuss
+*                          Riedlinger Str. 8
+*                          70327 Stuttgart
+*                          Germany
+*                          http://www.rothfuss-web.de
+*
+* @author    <a href="mailto:armin@rothfuss-web.de">Armin Rothfuss</a>
+*
+* Project    standard lib
+*
+* Customer   -
+*
+* @brief     C++ API: basic window types
+*
+* @file
+*/
+
+// --------------------------------------------------------------------------
+//
+// Common includes
+//
+#include <ostream>
+
+// --------------------------------------------------------------------------
+//
+// Library includes
+//
+#include "brush.h"
+
+
+namespace gui {
+
+  namespace draw {
+
+#ifdef WIN32
+    const brush brush::default_brush((core::brush_id)GetStockObject(WHITE_BRUSH));
+
+    brush::brush(HGDIOBJ id)
+      :id(static_cast<core::brush_id>(id))
+    {
+      GetObject(id, sizeof(core::brush_type), &info);
+    }
+
+    brush::brush(const draw::color& color, Style style)
+      : id(0)
+    {
+      info.lbColor = color;
+      info.lbStyle = style;
+      info.lbHatch = 0;
+      id = CreateBrushIndirect(&info);
+    }
+
+    brush::brush(const brush& rhs)
+      : id(CreateBrushIndirect(&rhs.info))
+      , info(rhs.info)
+    {}
+
+    brush::~brush() {
+      if (id) {
+        DeleteObject(id);
+        id = 0;
+      }
+    }
+
+    draw::color brush::color() const {
+      return info.lbColor;
+    }
+
+    brush::Style brush::style() const {
+      return (brush::Style)info.lbStyle;
+    }
+
+    brush brush::with_style(Style s) const {
+      core::brush_type newType = info;
+      newType.lbStyle = s;
+      return brush(CreateBrushIndirect(&newType));
+    }
+
+    brush brush::with_color(const draw::color& c) const {
+      core::brush_type newType = info;
+      newType.lbColor = c;
+      return brush(CreateBrushIndirect(&newType));
+    }
+
+    bool brush::operator== (const brush& rhs) const {
+      return ((info.lbColor == rhs.info.lbColor) &&
+              (info.lbStyle == rhs.info.lbStyle));
+    }
+
+#endif // WIN32
+#ifdef X11
+    const brush brush::default_brush;
+
+    brush::brush(const draw::color& color, Style style)
+      : m_color(color)
+      , m_style(style)
+    {}
+
+    brush::brush(const brush& rhs)
+      : m_color(rhs.m_color)
+      , m_style(rhs.m_style)
+    {}
+
+    brush::~brush() {
+    }
+
+    draw::color brush::color() const {
+      return m_color;
+    }
+
+    brush::Style brush::style() const {
+      return m_style;
+    }
+
+    brush brush::with_style(Style s) const {
+      return brush(m_color, s);
+    }
+
+    brush brush::with_color(const draw::color& c) const {
+      return brush(c, m_style);
+    }
+
+    bool brush::operator== (const brush& rhs) const {
+      return ((m_color == rhs.m_color) &&
+              (m_style == rhs.m_style));
+    }
+
+#endif // X11
+
+  }
+
+}
---
+++ gui/draw/sources/brush.h
@@ -0,0 +1,88 @@
+/**
+* @copyright (c) 2015-2016 Ing. Buero Rothfuss
+*                          Riedlinger Str. 8
+*                          70327 Stuttgart
+*                          Germany
+*                          http://www.rothfuss-web.de
+*
+* @author    <a href="mailto:armin@rothfuss-web.de">Armin Rothfuss</a>
+*
+* Project    gui lib
+*
+* Customer   -
+*
+* @brief     C++ API: brush definition
+*
+* @file
+*/
+
+#pragma once
+
+// --------------------------------------------------------------------------
+//
+// Common includes
+//
+
+// --------------------------------------------------------------------------
+//
+// Library includes
+//
+#include "gui_types.h"
+#include "color.h"
+
+
+namespace gui {
+
+  namespace draw {
+
+    struct /*immutable*/ brush {
+
+      const static brush default_brush;
+
+      enum Style {
+#ifdef WIN32
+        solid = BS_SOLID
+#elif X11
+        solid = FillSolid
+#else
+
+#pragma error "Unknown target system"
+
+#endif // WIN32
+      };
+
+      brush(const draw::color& = draw::color::black, Style = solid);
+      brush(const brush&);
+      ~brush();
+
+#ifdef WIN32
+      brush(HGDIOBJ);
+
+      inline operator core::brush_id() const {
+        return id;
+      }
+#endif // WIN32
+
+      draw::color color() const;
+      Style style() const;
+
+      brush with_style(Style) const;
+      brush with_color(const draw::color&) const;
+
+      bool operator== (const brush&) const;
+
+    private:
+#ifdef WIN32
+      core::brush_id id;
+      core::brush_type info;
+#endif // WIN32
+#ifdef X11
+      const draw::color m_color;
+      const Style m_style;
+#endif // X11
+
+    };
+
+  }
+
+}

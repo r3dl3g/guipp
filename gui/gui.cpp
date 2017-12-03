5697221c6319e9e6ebcd7843b157efac4778ef54
Armin Rothfuss <armin@rothfuss-web.de>
2017-12-03 23:11:40 +0100

    Add gui.cpp

---
+++ gui/gui.cpp
@@ -0,0 +1,31 @@
+/**
+ * @copyright (c) 2016-2017 Ing. Buero Rothfuss
+ *                          Riedlinger Str. 8
+ *                          70327 Stuttgart
+ *                          Germany
+ *                          http://www.rothfuss-web.de
+ *
+ * @author    <a href="mailto:armin@rothfuss-web.de">Armin Rothfuss</a>
+ *
+ * Project    standard lib
+ *
+ * Customer   -
+ *
+ * @brief     C++ API: dll defines
+ *
+ * @file
+ */
+
+// --------------------------------------------------------------------------
+//
+// Library includes
+//
+#include <gui/core/logger.h>
+
+
+#ifdef GUIPP_EXPORT
+#define NO_EXPORT
+
+DEFINE_LOGGING_CORE(NO_EXPORT)
+#endif // GUIPP_EXPORT
+

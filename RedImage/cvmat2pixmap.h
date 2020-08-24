283aa906f21354d20cf1e4e4400c123a562e5236
Armin Rothfuß <armin@rothfuss-web.de>
2020-08-24 09:58:55 +0200

    Move RedImage, ShowBtrfs and TimeTracker to standalone projects. Move raspi specific apps to raspi++. Move Apps from test to samples

--- RedImage/cvmat2pixmap.h
+++
@@ -1,7 +0,0 @@
-
-#pragma once
-
-#include "gui/draw/bitmap.h"
-#include <opencv2/imgproc/imgproc.hpp>
-
-gui::draw::pixmap cvMat2pixmap (const cv::Mat& source);
--- RedImage/over_view.cpp
+++
@@ -1,3 +0,0 @@
-
-#include "over_view.h"
-

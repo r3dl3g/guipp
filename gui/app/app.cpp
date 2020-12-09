8ebaa664c0ba22a478854fc1efbb40a66553ee02
Armin Rothfuss <armin@rothfuss-web.de>
2020-12-09 22:25:31 +0100

    Disable Qt High Dpi Scaling

--- gui/app/app.cpp
+++ gui/app/app.cpp
@@ -79,12 +79,14 @@ int main (int argc, char* argv[]) {
   gui::core::global::init(XOpenDisplay(display));
 #endif // GUIPP_X11
 #ifdef GUIPP_QT
+# ifdef GUIPP_QT_HIDPI
   if (QApplication::testAttribute(Qt::AA_EnableHighDpiScaling)) {
     clog::info() << "Qt HighDpiScaling is enabled";
   } else {
     clog::info() << "Qt enable HighDpiScaling";
     QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
   }
+# endif // GUIPP_QT_HIDPI
   QApplication qapplication(argc, argv);
   gui::core::global::init(&qapplication);
 #endif // GUIPP_QT

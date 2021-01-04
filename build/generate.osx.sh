381b43d1742f0b419409e4e6a2eedfab9f8c9b09
Armin Rothfuss <armin@Armins-iMac-Pro.local>
2021-01-04 17:39:26 +0100

    Update logging and util. Change generate for osx

--- build/generate.osx.sh
+++ build/generate.osx.sh
@@ -2,15 +2,15 @@
 builddir=./$(c++ --version | { read first rest ; echo $first ; })$(c++ -dumpversion)
 prjdir=$PWD/..
 
-mkdir -p $builddir
-pushd $builddir
-cmake "$prjdir" -G"Xcode" -DCMAKE_CONFIGURATION_TYPES=Debug;Release
-popd
-
-#mkdir -p $builddir/debug
-#pushd $builddir/debug
-#cmake "$prjdir" -G"Unix Makefiles" -DCMAKE_BUILD_TYPE="Debug"
+#mkdir -p $builddir/xcode
+#pushd $builddir/xcode
+#cmake "$prjdir" -G"Xcode" -DCMAKE_CONFIGURATION_TYPES=Debug;Release -DGUIPP_USE_QT=ON
 #popd
+
+mkdir -p $builddir/debug
+pushd $builddir/debug
+cmake "$prjdir" -G"Unix Makefiles" -DCMAKE_BUILD_TYPE="Debug" -DGUIPP_USE_QT=ON -DCMAKE_PREFIX_PATH=/usr/local/opt/qt5
+popd
 
 #mkdir -p $builddir/release
 #pushd $builddir/release

2e6c05d5e1e6dd72cb215aab502360ab69df0464
Armin Rothfuss <armin@rothfuss-web.de>
2021-01-06 17:50:44 +0100

    Optimize cmake files to run also with cmake 3.10 (ubuntu 18). Fix Mingw linking.

--- build/generate.osx.sh
+++ build/generate.osx.sh
@@ -1,17 +1,4 @@
 
-builddir=./$(c++ --version | { read first rest ; echo $first ; })$(c++ -dumpversion)
-prjdir=$PWD/..
+. ./generate.sh
 
-build () {
-  mkdir -p $builddir/$1
-  pushd $builddir/$1
-  shift
-  type=$1
-  shift
-  cmake "$prjdir" -G"Unix Makefiles" -DCMAKE_BUILD_TYPE="$type" -DGUIPP_USE_QT=ON -DCMAKE_PREFIX_PATH=/usr/local/opt/qt5 $*
-  popd
-}
-
-build release-static Release -DGUIPP_BUILD_STATIC_MODULE_LIBS=ON
-build release-shared Release -DGUIPP_BUILD_SHARED_MODULE_LIBS=ON
-build release Release
+generate_all osx -DGUIPP_USE_QT=ON -DCMAKE_PREFIX_PATH=/usr/local/opt/qt5 $*
--- build/generate.sh
+++ build/generate.sh
@@ -1,15 +1,31 @@
 
-builddir=./$(g++ --version | { read first rest ; echo $first ; })$(g++ -dumpversion)
+if [ "$CXX" == "" ]; then
+  builddir=./$(c++ --version | { read first rest ; echo $first ; })$(c++ -dumpversion)
+else
+  builddir=./$($CXX --version | { read first rest ; echo $first ; })$($CXX -dumpversion)
+fi
+
 prjdir=$PWD/..
 
-#mkdir -p $builddir/debug
-#pushd $builddir/debug
-#cmake "$prjdir" -G"Unix Makefiles" -DCMAKE_BUILD_TYPE="Debug" -DCMAKE_INSTALL_PREFIX=./dist
-##-DCMAKE_MAKE_PROGRAM="gmake" 
-#popd
+generate () {
+  mkdir -p $builddir/$1
+  pushd $builddir/$1
+  shift
+  type=$1
+  shift
+  cmake "$prjdir" -G"Unix Makefiles" -DCMAKE_BUILD_TYPE="$type" $*
+  popd
+}
 
-mkdir -p $builddir/release
-pushd $builddir/release
-cmake "$prjdir" -G"Unix Makefiles" -DCMAKE_BUILD_TYPE="Release" -DCMAKE_INSTALL_PREFIX=./dist -DBUILD_FOR_ARM=Off
-cmake --build . --  -j
-popd
+generate_all () {
+  prefix=$1
+  shift
+
+  generate $prefix-release-static Release -DGUIPP_BUILD_STATIC_MODULE_LIBS=ON $*
+  generate $prefix-release-shared Release -DGUIPP_BUILD_SHARED_MODULE_LIBS=ON $*
+  generate $prefix-release Release $*
+  generate $prefix-debug-static Debug -DGUIPP_BUILD_STATIC_MODULE_LIBS=ON $*
+  generate $prefix-debug-shared Debug -DGUIPP_BUILD_SHARED_MODULE_LIBS=ON $*
+  generate $prefix-debug Debug $*
+
+}

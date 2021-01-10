d05f24ce510b97e065739cbcb871d0bb3fcd4702
Armin Rothfuss <armin@Armins-iMac-Pro.local>
2021-01-11 00:24:06 +0100

    Fix Apple build

--- build/generate-osx.sh
+++ build/generate-osx.sh
@@ -1,7 +1,8 @@
 
 . ./.generate.sh
 
-#QTDIR=/usr/local/opt/qt5
-QTDIR=../qt/dist
+QTDIR=/usr/local/opt/qt5
+#QTDIR=../qt/dist
 
-generate_all osx -DGUIPP_USE_QT=ON -DCMAKE_PREFIX_PATH=$QTDIR -DGUIPP_USE_BOOST=ON $*
+generate_all osx -DGUIPP_USE_QT=ON  -DCMAKE_PREFIX_PATH=$QTDIR $*
+# -DGUIPP_USE_BOOST=ON

f513930898fc9679f00e0a4ac46803ca95dc38d3
Armin Rothfuss <armin@rothfuss-web.de>
2021-04-09 17:54:18 +0200

    Put everyting in namespace animation. Add push_from and pop_to.

--- gui/win/animation.cpp
+++ gui/win/animation.cpp
@@ -27,84 +27,101 @@ namespace gui {
 
   namespace win {
 
-    // --------------------------------------------------------------------------
-    animation::animation (window& win, std::chrono::milliseconds duration, int fps, float f0, float f1)
-      : f0(f0)
-      , delta(f1 - f0)
-      , duration(duration)
-      , repeater(win, std::max(std::chrono::milliseconds(1000) / fps, std::chrono::milliseconds(1))) {
-    }
+    namespace animation {
+      // --------------------------------------------------------------------------
+      engine::engine (window& win, std::chrono::milliseconds duration, int fps, float f0, float f1)
+        : f0(f0)
+        , delta(f1 - f0)
+        , duration(duration)
+        , repeater(win, std::max(std::chrono::milliseconds(1000) / fps, std::chrono::milliseconds(1)))
+        , animation_disabled(false)
+      {}
 
-    void animation::start (finish_t fin) {
-      stop();
-      const auto start_time = std::chrono::system_clock::now();
-      repeater.start([&, fin, start_time] () {
-        const auto now = std::chrono::system_clock::now();
-        const float f = f0 + delta * (now - start_time) / duration;
-        for(auto& a : animations) {
-          a(f);
-        }
-        if (now >= start_time + duration) {
-          repeater.stop();
+      void engine::start (finish_t fin) {
+        if (animation_disabled) {
+          for(auto& a : animations) {
+            a(f0 + delta);
+          }
+          clear();
           if (fin) {
             fin();
           }
+        } else {
+          stop();
+          const auto start_time = std::chrono::system_clock::now();
+          repeater.start([&, fin, start_time] () {
+            const auto now = std::chrono::system_clock::now();
+            const float f = f0 + delta * (now - start_time) / duration;
+            for(auto& a : animations) {
+              a(f);
+            }
+            if (now >= start_time + duration) {
+              repeater.stop();
+              clear();
+              if (fin) {
+                fin();
+              }
+            }
+          });
         }
-      });
-    }
-
-    void animation::stop () {
-      repeater.stop();
-      repeater.wait_for_finish();
-    }
-
-    void animation::add (const animation_t& a) {
-      animations.push_back(a);
-    }
-
-    void animation::add (animation_t&& a) {
-      animations.push_back(std::move(a));
-    }
-
-    void animation::clear () {
-      animations.clear();
-    }
-
-    // --------------------------------------------------------------------------
-    move_animation::move_animation (const window_ptr& win,
-                                    const core::point& from,
-                                    const core::point& to)
-      : win(win)
-      , from(from)
-      , diff(to - from)
-    {}
-
-    void move_animation::operator () (float f) {
-      if (win->is_valid()) {
-        win->position(from + diff * f);
-        win->set_visible(true);
-        win->get_parent()->invalidate();
       }
-    }
 
-    // --------------------------------------------------------------------------
-    size_animation::size_animation (const window_ptr& win,
-                                    const core::size& from,
-                                    const core::size& to)
-      : win(win)
-      , from(from)
-      , diff(to - from)
-    {}
-
-    void size_animation::operator () (float f) {
-      if (win->is_valid()) {
-        win->resize(from + diff * f);
-        win->set_visible(true);
-        win->get_parent()->invalidate();
+      void engine::stop () {
+        repeater.stop();
+        repeater.wait_for_finish();
       }
-    }
 
+      void engine::add (const animation_t& a) {
+        animations.push_back(a);
+      }
+
+      void engine::add (animation_t&& a) {
+        animations.push_back(std::move(a));
+      }
+
+      void engine::clear () {
+        animations.clear();
+      }
+
+      void engine::disable_animation (bool disable) {
+        animation_disabled = disable;
+      }
+
+      // --------------------------------------------------------------------------
+      move::move (const window_ptr& win,
+                  const core::point& from,
+                  const core::point& to)
+        : win(win)
+        , from(from)
+        , diff(to - from)
+      {}
+
+      void move::operator () (float f) {
+        if (win->is_valid()) {
+          win->position(from + diff * f);
+          win->set_visible();
+          win->get_parent()->invalidate();
+        }
+      }
+      // --------------------------------------------------------------------------
+      size::size (const window_ptr& win,
+                  const core::size& from,
+                  const core::size& to)
+        : win(win)
+        , from(from)
+        , diff(to - from)
+      {}
+
+      void size::operator () (float f) {
+        if (win->is_valid()) {
+          win->resize(from + diff * f);
+          win->set_visible();
+          win->get_parent()->invalidate();
+        }
+      }
+
+    } // namespace animation
     // --------------------------------------------------------------------------
-  } // win
+  } // namespace win
 
-} // gui
+} // namespace gui
--- gui/win/animation.h
+++ gui/win/animation.h
@@ -31,67 +31,163 @@
 // Library includes
 //
 #include <gui/win/background_repeater.h>
-#include <gui/core/point.h>
+#include <gui/core/rectangle.h>
 
 
 namespace gui {
 
   namespace win {
 
+    namespace animation {
+
+      // --------------------------------------------------------------------------
+      struct GUIPP_WIN_EXPORT engine {
+        typedef std::function<void(float)> animation_t;
+        typedef std::function<void()> finish_t;
+
+        engine (window& win, std::chrono::milliseconds duration, int fps = 50, float f0 = 0, float f1 = 1);
+
+        void start (finish_t fin);
+        void stop ();
+
+        void add (const animation_t& a);
+        void add (animation_t&& a);
+
+        void clear ();
+        void disable_animation (bool disable);
+
+      private:
+        float f0;
+        float delta;
+        float step;
+        std::chrono::milliseconds duration;
+        background_repeater repeater;
+        std::vector<animation_t> animations;
+        bool animation_disabled;
+      };
+
+      // --------------------------------------------------------------------------
+      typedef std::shared_ptr<win::window> window_ptr;
+
+      // --------------------------------------------------------------------------
+      struct GUIPP_WIN_EXPORT move {
+
+        move (const window_ptr& win, const core::point& from, const core::point& to);
+
+        void operator () (float f);
+
+      protected:
+        window_ptr win;
+        core::point from;
+        core::point diff;
+      };
+
+      // --------------------------------------------------------------------------
+      struct GUIPP_WIN_EXPORT size {
+
+        size (const window_ptr& win, const core::size& from, const core::size& to);
+
+        void operator () (float f);
+
+      protected:
+        window_ptr win;
+        core::size from;
+        core::size diff;
+      };
+
+      // --------------------------------------------------------------------------
+      template<typename O, typename I = O>
+      struct dual {
+        dual (O&& out, I&& in)
+          : out(std::move(out))
+          , in(std::move(in))
+        {}
+
+        void operator () (float f) {
+          out(f);
+          in(f);
+        }
+
+      protected:
+        O out;
+        I in;
+      };
+
+      // --------------------------------------------------------------------------
+      enum class position {
+        left,
+        right,
+        top,
+        bottom
+      };
+
+      // --------------------------------------------------------------------------
+      template<position T>
+      core::point in_start_point (const core::rectangle& r);
+
+      template<>
+      inline core::point in_start_point<position::left> (const core::rectangle& r) {
+        return r.top_left().dx(-r.width());
+      }
+
+      template<>
+      inline core::point in_start_point<position::right> (const core::rectangle& r) {
+        return r.top_right();
+      }
+
+      template<>
+      inline core::point in_start_point<position::top> (const core::rectangle& r) {
+        return r.top_left().dy(-r.height());
+      }
+
+      template<>
+      inline core::point in_start_point<position::bottom> (const core::rectangle& r) {
+        return r.bottom_left();
+      }
+
+      // --------------------------------------------------------------------------
+      template<position T>
+      core::point out_end_point (const core::rectangle& r);
+
+      template<>
+      inline core::point out_end_point<position::left> (const core::rectangle& r) {
+        return r.top_right();
+      }
+
+      template<>
+      inline core::point out_end_point<position::right> (const core::rectangle& r) {
+        return r.top_left().dx(-r.width() / 10);
+      }
+
+      template<>
+      inline core::point out_end_point<position::top> (const core::rectangle& r) {
+        return r.bottom_left();
+      }
+
+      template<>
+      inline core::point out_end_point<position::bottom> (const core::rectangle& r) {
+        return r.top_left().dy(-r.height() / 10);
+      }
+
+      // --------------------------------------------------------------------------
+      template<position T>
+      dual<move> push_from (const window_ptr& out,
+                            const window_ptr& in,
+                            const core::rectangle& r) {
+        return {{out, r.top_left(), out_end_point<T>(r)},
+          {in, in_start_point<T>(r), r.top_left()}};
+      }
+      // --------------------------------------------------------------------------
+      template<position T>
+      dual<move> pop_to (const window_ptr& out,
+                         const window_ptr& in,
+                         const core::rectangle& r) {
+        return {{out, r.top_left(), in_start_point<T>(r)},
+          {in, out_end_point<T>(r), r.top_left()}};
+      }
+
+    } // namespace animation
     // --------------------------------------------------------------------------
-    struct animation {
-      typedef std::function<void(float)> animation_t;
-      typedef std::function<void()> finish_t;
+  } // namespace win
 
-      animation (window& win, std::chrono::milliseconds duration, int fps = 50, float f0 = 0, float f1 = 1);
-
-      void start (finish_t fin);
-      void stop ();
-
-      void add (const animation_t& a);
-      void add (animation_t&& a);
-
-      void clear ();
-
-    private:
-      float f0;
-      float delta;
-      float step;
-      std::chrono::milliseconds duration;
-      background_repeater repeater;
-      std::vector<animation_t> animations;
-    };
-
-    // --------------------------------------------------------------------------
-    typedef std::shared_ptr<win::window> window_ptr;
-
-    // --------------------------------------------------------------------------
-    struct move_animation {
-
-      move_animation (const window_ptr& win, const core::point& from, const core::point& to);
-
-      void operator () (float f);
-
-    protected:
-      window_ptr win;
-      core::point from;
-      core::point diff;
-    };
-
-    // --------------------------------------------------------------------------
-    struct size_animation {
-
-      size_animation (const window_ptr& win, const core::size& from, const core::size& to);
-
-      void operator () (float f);
-
-    protected:
-      window_ptr win;
-      core::size from;
-      core::size diff;
-    };
-
-    // --------------------------------------------------------------------------
-  } // win
-
-} // gui
+} // namespace gui

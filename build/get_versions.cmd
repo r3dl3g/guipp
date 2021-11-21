f754ad26edf49c061dada8c8d62a7e91f2679978
Armin Rothfuss <armin@rothfuss-web.de>
2021-11-21 17:39:48 +0100

    Update get_version.cmd to studio 2022.

--- build/get_versions.cmd
+++ build/get_versions.cmd
@@ -4,32 +4,48 @@ set BUILD_DIR=.
 set GENERATOR=Visual Studio
 set CLANG_TOOLSET=undefined
 
-if "%VisualStudioVersion%" == "15.0" (
-	echo Visual Studio Version 2017
-	set BUILD_DIR=%BUILD_DIR%\vc15
-	set GENERATOR=%GENERATOR% 15 2017 Win64
+if "%VisualStudioVersion%" == "17.0" (
+	echo Visual Studio Version 2022
+	set BUILD_DIR=%BUILD_DIR%\vc17
+	set GENERATOR=%GENERATOR% 17 2022
 	REM set CLANG_TOOLSET=v141_clang_c2
 	set CLANG_TOOLSET=LLVM-vs2014
 ) else (
-	if "%VisualStudioVersion%" == "14.0" (
-		echo Visual Studio Version 2015
-		set BUILD_DIR=%BUILD_DIR%\vc14
-		set GENERATOR=%GENERATOR% 14 2015 Win64
-		set CLANG_TOOLSET=v140_clang_c2
+	if "%VisualStudioVersion%" == "16.0" (
+		echo Visual Studio Version 2019
+		set BUILD_DIR=%BUILD_DIR%\vc16
+		set GENERATOR=%GENERATOR% 16 2019
+		REM set CLANG_TOOLSET=v141_clang_c2
+		set CLANG_TOOLSET=LLVM-vs2014
 	) else (
-		if "%VisualStudioVersion%" == "12.0" (
-			echo Visual Studio Version 2013
-			set BUILD_DIR=%BUILD_DIR%\vc12
-			set GENERATOR=%GENERATOR% 12 2013 Win64
+		if "%VisualStudioVersion%" == "15.0" (
+			echo Visual Studio Version 2017
+			set BUILD_DIR=%BUILD_DIR%\vc15
+			set GENERATOR=%GENERATOR% 15 2017 Win64
+			REM set CLANG_TOOLSET=v141_clang_c2
+			set CLANG_TOOLSET=LLVM-vs2014
 		) else (
-			if NOT "%VisualStudioVersion%" == "" (
-				echo Unbekannte Visual Studio Version '%VisualStudioVersion%'!
+			if "%VisualStudioVersion%" == "14.0" (
+				echo Visual Studio Version 2015
+				set BUILD_DIR=%BUILD_DIR%\vc14
+				set GENERATOR=%GENERATOR% 14 2015 Win64
+				set CLANG_TOOLSET=v140_clang_c2
 			) else (
-				echo Skript muss in einer Visual Studio Developer Eingabeaufforderung ausgefuehrt werden!
+				if "%VisualStudioVersion%" == "12.0" (
+					echo Visual Studio Version 2013
+					set BUILD_DIR=%BUILD_DIR%\vc12
+					set GENERATOR=%GENERATOR% 12 2013 Win64
+				) else (
+					if NOT "%VisualStudioVersion%" == "" (
+						echo Unbekannte Visual Studio Version '%VisualStudioVersion%'!
+					) else (
+						echo Skript muss in einer Visual Studio Developer Eingabeaufforderung ausgefuehrt werden!
+					)
+					goto end
+				)
 			)
-			goto end
 		)
- 	)
+	)
 )
 
 echo Build dir:%BUILD_DIR%

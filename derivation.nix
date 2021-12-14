7782c3488ccc4ebf22e5636b143c53ce5b3f6729
Armin Rothfuss <armin@rothfuss-web.de>
2021-12-14 18:58:42 +0100

    Update cmake version. Add derivation.nix Beautify CMakeList.txt

---
+++ derivation.nix
@@ -0,0 +1,46 @@
+# https://discourse.nixos.org/t/how-to-use-a-nix-derivation-from-a-local-folder/5498/4
+
+{ stdenv, lib, pkgs } :
+
+stdenv.mkDerivation {
+  pname = "guipp";
+  version = "1.0.1";
+
+  src = ./.;
+
+  nativeBuildInputs = with pkgs; [ cmake ];
+
+  buildInputs = with pkgs; [
+    xcbutilxrm
+    xorg.libX11.dev
+    xorg.libXft.dev
+    xorg.libxcb.dev
+    xorg.libXdmcp.dev
+    (pkgs.callPackage ../util/derivation.nix {})
+    (pkgs.callPackage ../logging/derivation.nix {})
+    (pkgs.callPackage ../testing/derivation.nix {})
+  ];
+
+  enableParallelBuilding = true;
+  separateDebugInfo = true;
+
+  outputs = [ "dev" "out" ];
+
+  cmakeFlags = [
+    "-DGUIPP_TESTS=OFF"
+    "-DGUIPP_CONFIG_INSTALL=ON"
+    "-DLOGGING_CONFIG_INSTALL=ON"
+    "-DUTIL_CONFIG_INSTALL=ON"
+    "-DTESTING_CONFIG_INSTALL=ON"
+#    "-DGUIPP_BUILD_DEPENDENT_LIBS=ON"
+  ];
+
+  meta = with lib; {
+    description = "A pure c++ model-view-control based application framework";
+    homepage = "https://github.com/r3dl3g/guipp";
+    changelog = "https://github.com/r3dl3g/guipp/releases/tag/v${version}";
+    license = licenses.mit;
+    maintainers = with maintainers; [ r3dl3g ];
+    platforms = with platforms; linux;
+  };
+}

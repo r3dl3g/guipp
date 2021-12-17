2d9ce2fa108c81d30fdb6711d74f440184a7f02f
Armin Rothfuss <armin@rothfuss-web.de>
2021-12-17 04:40:47 +0100

    Update logging lib. Add needed defines to generate.

--- build/.generate.sh
+++ build/.generate.sh
@@ -14,7 +14,7 @@ generate () {
 
   mkdir -p $builddir
   pushd $builddir
-  cmake "$prjdir" -G"Unix Makefiles" -DCMAKE_BUILD_TYPE="$type" $*
+  cmake "$prjdir" -G"Unix Makefiles" -DCMAKE_BUILD_TYPE="$type" -DGUIPP_BUILD_DEPENDENT_LIBS=ON -DGUIPP_SAMPLES=ON -DGUIPP_TESTS=ON -DGUIPP_TOOLS=ON $*
   popd
 }
 
@@ -22,11 +22,11 @@ generate_all () {
   prefix=$1
   shift
 
-  generate $prefix-release-static Release -DGUIPP_BUILD_STATIC_MODULE_LIBS=ON $*
-  generate $prefix-release-shared Release -DGUIPP_BUILD_SHARED_MODULE_LIBS=ON $*
+  generate $prefix-release-static Release -DGUIPP_BUILD_STATIC_MODULE_LIBS=ON -DLOGGING_BUILD_STATIC_MODULE_LIB=ON -DUTIL_BUILD_STATIC_MODULE_LIB=ON $*
+  generate $prefix-release-shared Release -DGUIPP_BUILD_SHARED_MODULE_LIBS=ON -DLOGGING_BUILD_STATIC_MODULE_LIB=OFF -DUTIL_BUILD_STATIC_MODULE_LIB=OFF $*
   generate $prefix-release Release $*
-  generate $prefix-debug-static Debug -DGUIPP_BUILD_STATIC_MODULE_LIBS=ON $*
-  generate $prefix-debug-shared Debug -DGUIPP_BUILD_SHARED_MODULE_LIBS=ON $*
+  generate $prefix-debug-static Debug -DGUIPP_BUILD_STATIC_MODULE_LIBS=ON -DLOGGING_BUILD_STATIC_MODULE_LIB=ON -DUTIL_BUILD_STATIC_MODULE_LIB=ON $*
+  generate $prefix-debug-shared Debug -DGUIPP_BUILD_SHARED_MODULE_LIBS=ON -DLOGGING_BUILD_STATIC_MODULE_LIB=OFF -DUTIL_BUILD_STATIC_MODULE_LIB=OFF $*
   generate $prefix-debug Debug $*
 
 }
--- build/generate-gcc.sh
+++ build/generate-gcc.sh
@@ -1,5 +1,5 @@
 
 . ./.generate.sh
 
-generate_all gcc -DGUIPP_STANDALONE_BUILD=ON -DGUIPP_USE_XCB=ON $*
-#generate gcc-Debug Debug -DGUIPP_STANDALONE_BUILD=ON -DGUIPP_USE_XCB=ON $*
+generate_all gcc -DGUIPP_USE_XCB=ON $*
+#generate gcc-Debug Debug -DGUIPP_USE_XCB=ON $*

8550ff1de0a0ac31f7741e49ced3ee6fbfed282b
Armin Rothfuss <armin@rothfuss-web.de>
2022-06-08 18:55:48 +0200

    Version 1.2.1

--- derivation-qt.nix
+++ derivation-qt.nix
@@ -4,7 +4,7 @@
 
 stdenv.mkDerivation rec {
   pname = "guipp-qt";
-  version = "1.2.0";
+  version = "1.2.1";
 
   src = ./.;
 
--- derivation.nix
+++ derivation.nix
@@ -4,7 +4,7 @@
 
 stdenv.mkDerivation rec {
   pname = "guipp";
-  version = "1.2.0";
+  version = "1.2.1";
 
   src = ./.;
 

dad1595474e442a9c119ad6e0830b6709c237449
Armin Rothfuss <armin@rothfuss-web.de>
2023-11-17 16:38:30 +0100

    Use placeholder in sample app.

--- samples/SplitView2App/SplitView2App.cpp
+++ samples/SplitView2App/SplitView2App.cpp
@@ -4,6 +4,7 @@
 #include <gui/layout/border_layout.h>
 #include <gui/ctrl/split_view.h>
 #include <gui/ctrl/label.h>
+#include <gui/ctrl/button.h>
 
 // --------------------------------------------------------------------------
 int gui_main(const std::vector<std::string>& /*args*/) {
@@ -16,34 +17,56 @@ int gui_main(const std::vector<std::string>& /*args*/) {
 
   using label_t = basic_label<text_origin_t::center, draw::frame::raised_deep_relief>;
   using client_top = vertical_split_view<label_t, label_t>;
-  using client_mid = vertical_split_view<label_t, label_t, label_t>;
-  using client_bot = vertical_split_view<label_t, label_t, label_t, label_t>;
+  using client_mid = vertical_split_view<label_t, placeholder, label_t>;
+  using client_bot = vertical_split_view<label_t, text_button, text_button, label_t>;
   using client_single = vertical_split_view<label_t>;
   using client_t = horizontal_split_view<client_top, client_mid, client_bot, client_single>;
+
+  using center_deep = basic_label<text_origin_t::center, draw::frame::sunken_deep_relief>;
+  using center_raised = basic_label<text_origin_t::center, draw::frame::raised_deep_relief>;
+
+  center_deep deep_center_label([&] () { return ostreamfmt("Deep (" << deep_center_label.geometry() << ")"); });
+  center_raised raised_center_label([&] () { return ostreamfmt("Raised (" << raised_center_label.geometry() << ")"); });
 
   client_t client;
   layout_main_window<border::center_layout<>, client_t&> main(client);
 
+  auto& top = client.get<0>();
+  top.get<0>().set_text([&] () { return ostreamfmt("Left (" << top.get<0>().geometry() << ")"); });
+  top.get<1>().set_text([&] () { return ostreamfmt("Right (" << top.get<1>().geometry() << ")"); });
+
+  auto& mid = client.get<1>();
+  mid.get<0>().set_text([&] () { return ostreamfmt("Left (" << mid.get<0>().geometry() << ")"); });
+  mid.get<1>().set(&deep_center_label);
+  mid.get<2>().set_text([&] () { return ostreamfmt("Right (" << mid.get<2>().geometry() << ")"); });
+
+  auto& bot = client.get<2>();
+  bot.get<0>().set_text([&] () { return ostreamfmt("Left (" << bot.get<0>().geometry() << ")"); });
+  bot.get<1>().set_text("Raise");
+  bot.get<1>().on_clicked([&] () {
+    deep_center_label.set_visible(false);
+    mid.get<1>().set(&raised_center_label);
+    raised_center_label.set_visible();
+    mid.layout();
+  });
+  bot.get<2>().set_text("Deep");
+  bot.get<2>().on_clicked([&] () {
+    raised_center_label.set_visible(false);
+    mid.get<1>().set(&deep_center_label);
+    deep_center_label.set_visible();
+    mid.layout();
+  });
+  bot.get<3>().set_text([&] () { return ostreamfmt("Right (" << bot.get<3>().geometry() << ")"); });
+
+  auto& single = client.get<3>();
+  single.get<0>().set_text([&] () { return ostreamfmt("Single (" << single.get<0>().geometry() << ")"); });
+
   main.set_background(color::blue);
   main.on_create([&] () {
     client.create(main);
-    auto& top = client.get<0>();
-    top.get<0>().set_text([&] () { return ostreamfmt("Left (" << top.get<0>().geometry() << ")"); });
-    top.get<1>().set_text([&] () { return ostreamfmt("Right (" << top.get<1>().geometry() << ")"); });
-
-    auto& mid = client.get<1>();
-    mid.get<0>().set_text([&] () { return ostreamfmt("Left (" << mid.get<0>().geometry() << ")"); });
-    mid.get<1>().set_text([&] () { return ostreamfmt("Mid (" << mid.get<1>().geometry() << ")"); });
-    mid.get<2>().set_text([&] () { return ostreamfmt("Right (" << mid.get<2>().geometry() << ")"); });
-
-    auto& bot = client.get<2>();
-    bot.get<0>().set_text([&] () { return ostreamfmt("Left (" << bot.get<0>().geometry() << ")"); });
-    bot.get<1>().set_text([&] () { return ostreamfmt("Mid Left (" << bot.get<1>().geometry() << ")"); });
-    bot.get<2>().set_text([&] () { return ostreamfmt("Mid Right (" << bot.get<2>().geometry() << ")"); });
-    bot.get<3>().set_text([&] () { return ostreamfmt("Right (" << bot.get<3>().geometry() << ")"); });
-
-    auto& single = client.get<3>();
-    single.get<0>().set_text([&] () { return ostreamfmt("Single (" << single.get<0>().geometry() << ")"); });
+    // not necessary
+    // deep_center_label.create(mid);
+    raised_center_label.create(mid);
   });
 
   main.create({50, 50, 800, 600});

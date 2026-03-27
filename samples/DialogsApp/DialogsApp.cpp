e9656851e9620277be79c8e0dda5498cb86e59e6
Armin Rothfuss <armin@rothfuss-web.de>
2026-03-27 12:38:13 +0100

    Fix change from overlapped to container_window for child popup windows

--- samples/DialogsApp/DialogsApp.cpp
+++ samples/DialogsApp/DialogsApp.cpp
@@ -11,7 +11,7 @@ int gui_main(const std::vector<std::string>& /*args*/) {
   main_window main;
   main.create();
  
-  yes_no_dialog::ask(main, "Question", "This is a Yes/No dialog", "Yes", "no", [&] (overlapped_window& dlg, bool y) {
+  yes_no_dialog::ask(main, "Question", "This is a Yes/No dialog", "Yes", "no", [&] (container& dlg, bool y) {
     message_dialog::show(dlg, "Message", ostreamfmt("This is a message dialog\nYou choose: " << (y ? "Yes" : "No")), "ok");
   });
 

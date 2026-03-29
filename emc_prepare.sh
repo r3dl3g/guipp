204399632d752bd34d561b07aa05828f6a4205e1
Armin Rothfuss <armin@rothfuss-web.de>
2026-03-29 16:18:59 +0200

    Update build files

--- emc_prepare.sh
+++ emc_prepare.sh
@@ -3,6 +3,7 @@
 BUILD_PREFIX=~/.cache/build/emc
 INSTALL_PREFIX=~/.local/emc
 CMAKE_CMD="emcmake cmake"
+DEFINES=-DGUIPP_SAMPLES=ON
 
-prepare Debug -DVERBOSE=ON -DGUIPP_TESTS=ON -DGUIPP_SAMPLES=ON $@
+prepare Debug $@
 prepare Release $@

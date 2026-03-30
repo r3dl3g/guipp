7327c182260256b2fec2c4149848ebf6c9ce3abd
Armin Rothfuss <armin@rothfuss-web.de>
2026-03-30 11:58:07 +0200

    Add Text to Hello world

--- samples/HelloWorldApp/HelloWorldApp.cpp
+++ samples/HelloWorldApp/HelloWorldApp.cpp
@@ -1,16 +1,28 @@
 
 
 #include <gui/win/overlapped_window.h>
+#include <gui/draw/graphics.h>
+#include <gui/draw/drawers.h>
+#include <gui/draw/font.h>
 
 
 // --------------------------------------------------------------------------
 int gui_main(const std::vector<std::string>& /*args*/) {
+  using namespace gui;
   using namespace gui::win;
+  using namespace gui::draw;
 
   main_window  main;
 
   main.create({50, 50, 800, 600});
   main.on_destroy(&quit_main_loop);
+  main.on_paint(paint([&](graphics& graph){
+    graph.text(text_box("Hello world!",
+                        main.client_geometry(),
+                        text_origin_t::center),
+               font::system(), color::black);
+  }));
+
   main.set_title("Hello world");
   main.set_visible();
 

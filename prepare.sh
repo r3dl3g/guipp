5352b7d7c6651ae62faf18dc391c50048ee074ab
Armin Rothfuss <armin@rothfuss-web.de>
2026-04-02 09:22:35 +0200

    Remove GUIPP_SAMPLES=ON from default prepare

--- prepare.sh
+++ prepare.sh
@@ -1,13 +1,12 @@
 . ./.project_defines
 
 if [ "$PLATFORM" == "qt5" ]; then
-    DEFINES="-DGUIPP_SAMPLES=ON -DGUIPP_USE_QT5=ON"
+    DEFINES="-DGUIPP_USE_QT5=ON"
 elif [ "$PLATFORM" == "qt6" ]; then
-    DEFINES="-DGUIPP_SAMPLES=ON -DGUIPP_USE_QT6=ON"
+    DEFINES="-DGUIPP_USE_QT6=ON"
 elif [ "$PLATFORM" == "emc" ]; then
     INSTALL_PREFIX=~/.local/emc
     CMAKE_CMD="cmake --preset emc_debug"
-    DEFINES=-DGUIPP_SAMPLES=ON
 # elif [ "$PLATFORM" == "x11" ]; then
 fi
 
